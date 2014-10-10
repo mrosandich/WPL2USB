975848d4b60af5b4827193f31efbf405ede6e343
Mell Rosandich <github@ourace.com>
2014-10-10 15:42:50 -0500

    Initial Import
    This is the first import into GIT

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace WPL2USB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select WPL Directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(971, 34);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.progressBar1);
+            this.splitContainer1.Size = new System.Drawing.Size(971, 546);
+            this.splitContainer1.SplitterDistance = 508;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.linkLabel1);
+            this.splitContainer2.Panel1.Controls.Add(this.checkBox2);
+            this.splitContainer2.Panel1.Controls.Add(this.checkBox1);
+            this.splitContainer2.Panel1.Controls.Add(this.button2);
+            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            this.splitContainer2.Panel1.Controls.Add(this.button4);
+            this.splitContainer2.Panel1.Controls.Add(this.button3);
+            this.splitContainer2.Panel1.Controls.Add(this.textBox3);
+            this.splitContainer2.Panel1.Controls.Add(this.textBox2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer2.Size = new System.Drawing.Size(971, 508);
+            this.splitContainer2.SplitterDistance = 85;
+            this.splitContainer2.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(649, 49);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 27);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Copy Songs";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Select Destination Folder or Drive";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(202, 51);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(316, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(202, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(316, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(971, 419);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 524);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(971, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(35, 17);
+            this.toolStripStatusLabel1.Text = "Idle...";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(524, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Refresh List";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(649, 26);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(271, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Clean Names (use only A-Z, a-z, 0-9,_,- and spaces)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(649, 4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(319, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Use playlist name for destination folder (don\'t use artist/album) ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(768, 56);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(68, 13);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Visit Website";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 546);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "WPL 2 USB";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,389 @@
+﻿/* Created By:  
+ * Mell Rosandich
+ * Mell@ourace.com
+ * 2014-10-10
+ * http://ourace.com
+ * http://ourace.com/130-wpl2usb
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+
+
+
+
+namespace WPL2USB
+{
+    public partial class Form1 : Form
+    {
+
+        //Used for stopping the copy process at any time
+        bool isCopying = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox3.Text = Properties.Settings.Default.SelectWPLDirectory;
+            textBox2.Text = Properties.Settings.Default.SelectDestination;
+
+            if (Properties.Settings.Default.CleanNames == "True")
+            {
+                checkBox1.Checked = true;
+            }
+
+            if (Properties.Settings.Default.PlaylistOrArtist == "True")
+            {
+                checkBox2.Checked = true;
+            }
+           
+        }
+
+       
+        //This function takes a folder path
+        //it will iterate through all the wpl file types
+        //it will create the data grid view show counts
+        private void LoadWPLList(string SelectedPath)
+        {
+            int totalFilesCounted = 0;
+            progressBar1.Value = 0;
+            Application.DoEvents();
+
+            DataTable dt = new DataTable();
+            DataColumn PLNAME = new DataColumn("Play List", typeof(string));
+            PLNAME.ReadOnly = true;
+
+
+            DataColumn RemoteFolderName = new DataColumn("Folder To Copy To", typeof(string));
+            RemoteFolderName.ReadOnly = true;
+
+            DataColumn SongsInList = new DataColumn("Songs In List", typeof(string));
+            SongsInList.ReadOnly = true;
+
+            DataColumn SongsCopied = new DataColumn("Songs Copied", typeof(string));
+            SongsCopied.ReadOnly = false;
+
+            dt.Columns.Add(PLNAME);
+            dt.Columns.Add(RemoteFolderName);
+            dt.Columns.Add(SongsInList);
+            dt.Columns.Add(SongsCopied);
+
+            
+            List<string> Files = new List<string>(Directory.EnumerateFiles(SelectedPath, "*.wpl", SearchOption.TopDirectoryOnly));
+            foreach (var File in Files)
+            {
+                string DestNameFolder = File.Substring(SelectedPath.Length + 1);
+                DestNameFolder = DestNameFolder.Replace(".wpl", "");
+
+                int cpd = LoadWPL(File, DestNameFolder, false);
+                totalFilesCounted += cpd;
+                dt.Rows.Add(File, DestNameFolder, cpd.ToString(), "0");
+            }
+            dataGridView1.DataSource = dt;
+            dataGridView1.AutoResizeColumns();
+            progressBar1.Maximum = totalFilesCounted;
+
+
+        }//end LoadWPLList
+
+
+        //This is called to copy all files in the play list (wpl)
+        private void RunCopy()
+        {
+
+            string SelectedPath = textBox3.Text;
+            List<string> Files = new List<string>(Directory.EnumerateFiles(SelectedPath, "*.wpl", SearchOption.TopDirectoryOnly));
+            foreach (var File in Files)
+            {
+                if (isCopying == true)
+                {
+                    string DestNameFolder = File.Substring(SelectedPath.Length + 1);
+                    DestNameFolder = DestNameFolder.Replace(".wpl", "");
+                    int cpd = LoadWPL(File, DestNameFolder, true);
+                    UpdateGridCount(File, cpd);
+                }
+            }
+
+            toolStripStatusLabel1.Text = "Idle: Just finished copying.";
+            button4.Text = "Start Copy Songs";
+            isCopying = false;
+        }//end RunCopy
+
+
+        //As files are copied lets up data the totals in the grid view.
+        private void UpdateGridCount(string WPLPath, int FilesCopiedCnt)
+        {
+            //update the data grid count
+            int rowIndex = -1;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value.ToString().Equals(WPLPath))
+                {
+                    rowIndex = row.Index;
+                    break;
+                }
+            }
+            if (rowIndex != -1)
+            {
+
+                dataGridView1.Rows[rowIndex].Cells[3].Value = FilesCopiedCnt;
+            }
+        }//end UpdateGridCount
+
+
+        //This function loads the WPL file and find all the songs in the file
+        //This will copy the files to the selected folder
+        //returns how many files copied.
+        private int LoadWPL(string LoadPath,string ToPath,bool DoCopy)
+        {
+            //This is the count of files copied from the current WPL or files counted
+            int RetVal = 0;
+
+            //Create an XML document and load the WPL
+            XmlDocument document = new XmlDocument();
+            document.Load(LoadPath);
+
+            XmlNodeList MediaList = document.GetElementsByTagName("media");
+
+
+            List<string> sources = new List<string>();
+            for (int ix = 0; ix < MediaList.Count; ix++)
+            {
+                if (DoCopy == true && isCopying == true)
+                {
+
+
+                    //This is the path to the media file
+                    string FromPath = MediaList[ix].Attributes[0].Value.ToString();
+
+                    //this means a relative path to the wpl file
+                    //it is up one directory
+                    
+                    if (FromPath.Contains("..\\"))
+                    {
+
+                        var FilePartsRel = LoadPath.Split('\\');
+                        string FilePathNewRelative = ""; 
+                        int RelPathMinusFileAndFolder = FilePartsRel.Count() - 2;
+                        for (int fidx = 0; fidx < RelPathMinusFileAndFolder; fidx++)
+                        {
+                            FilePathNewRelative += FilePartsRel[fidx] + "\\";
+                        }
+                        FromPath = FromPath.Replace("..\\", FilePathNewRelative);
+                    }
+
+
+                    if (checkBox2.Checked == true)
+                    {
+                        //We just create the playlist name as a folder here.
+                        if (!System.IO.Directory.Exists(textBox2.Text + ToPath))
+                        {
+                            System.IO.Directory.CreateDirectory(textBox2.Text + ToPath);
+                        }
+                    }
+                    else
+                    {
+                        //we need to iterate down the path to create album artist
+                        //F:\Music\Green Day\American Idiot (Parental Advisory)\01 American Idiot.mp3
+                        //ToPath needs to be: Green Day\American Idiot (Parental Advisory)
+                        string ArtistAlbumPath = "";
+                        var FilePartsRel = FromPath.Split('\\');
+                        ArtistAlbumPath = FilePartsRel[FilePartsRel.Count() - 3] + "\\" + FilePartsRel[FilePartsRel.Count() - 2];
+                        ToPath = ArtistAlbumPath;
+
+                        if (checkBox1.Checked == true)
+                        {
+                            FilePartsRel[FilePartsRel.Count() - 3] = Regex.Replace(FilePartsRel[FilePartsRel.Count() - 3], "[^a-zA-Z0-9\\s\\-\\\\\\.]", "");
+                            FilePartsRel[FilePartsRel.Count() - 2] = Regex.Replace(FilePartsRel[FilePartsRel.Count() - 2], "[^a-zA-Z0-9\\s\\-\\\\\\.]", "");
+                        }
+
+
+
+
+                        if (!System.IO.Directory.Exists(textBox2.Text + FilePartsRel[FilePartsRel.Count() - 3]))
+                        {
+                            System.IO.Directory.CreateDirectory(textBox2.Text + FilePartsRel[FilePartsRel.Count() - 3]);
+                        }
+                        if (!System.IO.Directory.Exists(textBox2.Text + FilePartsRel[FilePartsRel.Count() - 3] + "\\" + FilePartsRel[FilePartsRel.Count() - 2]))
+                        {
+                            System.IO.Directory.CreateDirectory(textBox2.Text + FilePartsRel[FilePartsRel.Count() - 3] + "\\" + FilePartsRel[FilePartsRel.Count() - 2]);
+                        }
+                    }
+
+
+                    var Fileparts = FromPath.Split('\\');
+                    string FName = Fileparts[Fileparts.Count() - 1];
+
+                    //ToPath  will be the PlayListName or Alubum/Artist
+                    //Fname is the name of the file.
+                    string DestPathPart = ToPath + "\\" + FName;
+
+                    //if clean names, lets remove everything but a-z A_Z space and - and _ and \
+                    if (checkBox1.Checked == true)
+                    {
+                        DestPathPart = Regex.Replace(DestPathPart, "[^a-zA-Z0-9\\s\\-\\\\\\.]", "");
+                    }
+                    string FullpathTo = textBox2.Text + DestPathPart;
+
+
+                    if (File.Exists(FromPath))
+                    {
+                        if (!File.Exists(FullpathTo))
+                        {
+
+
+                            toolStripStatusLabel1.Text = "Copying: " + FromPath;
+                            File.Copy(FromPath, FullpathTo, true);
+
+                            Application.DoEvents();
+                        }
+                        else
+                        {
+                            toolStripStatusLabel1.Text = "Already Exsist: " + FromPath;
+                        }
+                        if (isCopying == true)
+                        {
+                            progressBar1.Value = progressBar1.Value + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (isCopying == true)
+                        {
+                            toolStripStatusLabel1.Text = "Skipping: " + FromPath;
+                            progressBar1.Value = progressBar1.Value + 1;
+                            Application.DoEvents();
+                        }
+                    }
+                   
+                }
+                RetVal++;
+            }//end for loop
+
+            return RetVal;
+        } //end LoadWPL
+
+
+       
+
+
+      
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                LoadWPLList(folderBrowserDialog1.SelectedPath);
+                textBox3.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }//end button1_Click
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadWPLList(textBox3.Text);
+        }//button2_Click
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+                if (textBox2.Text.Substring((textBox2.Text.Length - 1), 1) != "\\")
+                {
+                    textBox2.Text += "\\";
+                }
+            }
+        }//end button3_Click
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if( Directory.Exists(textBox2.Text) && Directory.Exists(textBox3.Text) )
+            {
+                if (isCopying == true)
+                {
+                    button4.Text = "Start Copy Songs";
+                    isCopying = false;
+                    if( toolStripStatusLabel1.Text.Contains("\\") )
+                    {
+                        toolStripStatusLabel1.Text = "You hit Stop: Last file copied was: " + toolStripStatusLabel1.Text;
+                    }
+                }
+                else
+                {
+                    isCopying = true;
+                    button4.Text = "Stop Copy Songs";
+                    RunCopy();
+                }
+            }
+            else
+            {
+                string Errormessage = "";
+                if (!Directory.Exists(textBox2.Text) )
+                {
+                    Errormessage = "A Valid Destination Folder / Drive is Required.";
+                }
+
+                if (!Directory.Exists(textBox3.Text))
+                {
+                    Errormessage = "A Valid WPL Directory is Required.";
+                }
+                if (!Directory.Exists(textBox2.Text) && !Directory.Exists(textBox3.Text))
+                {
+                    Errormessage = "A Valid Destination Folder / Drive is Required." + Environment.NewLine + "A Valid WPL Directory is Required.";
+                }
+                MessageBox.Show(Errormessage);
+
+            }
+        }//end button4_Click
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.SelectDestination = textBox2.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.SelectWPLDirectory = textBox3.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            Properties.Settings.Default.CleanNames = checkBox1.Checked.ToString() ;
+            Properties.Settings.Default.Save();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.PlaylistOrArtist = checkBox2.Checked.ToString();
+            Properties.Settings.Default.Save();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://ourace.com/130-wpl2usb");
+        } 
+    }
+}
